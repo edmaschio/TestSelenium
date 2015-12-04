a83cde6d8ec280b75c363ed9052916f3fd3f1ebe
Ederson Maschio <edmaschio@gmail.com>
2015-12-03 23:34:09 -0200

    Teste inicial do Selenium

---
+++ TestSelenium/Program.cs
@@ -0,0 +1,52 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+
+using OpenQA.Selenium.Support.UI;
+
+namespace TestSelenium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new FirefoxDriver();
+
+            driver.Navigate().GoToUrl("http://consultacadastral.inss.gov.br/Esocial/pages/index.xhtml");
+
+            IWebElement query = driver.FindElement(By.Id("indexForm1"));
+
+            query.Click();
+
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until((d) => { return d.Title.ToLower().StartsWith("consulta qualificação"); });
+
+            query = driver.FindElement(By.Id("formQualificacaoCadastral:nome"));
+            query.SendKeys("Ederson Tadeu Maschio");
+
+            query = driver.FindElement(By.Id("formQualificacaoCadastral:dataNascimento"));
+            query.SendKeys("28101981");
+
+            wait.Until((d) => { return d.Title.ToLower().StartsWith("consulta qualificação"); });
+            query = driver.FindElement(By.Id("formQualificacaoCadastral:cpf"));
+            query.SendKeys("22216791806");
+
+            query = driver.FindElement(By.Id("formQualificacaoCadastral:nis"));
+            query.SendKeys("12714688162");
+
+            query.Submit();
+
+
+
+            Console.WriteLine("O título da página é: " + driver.Title);
+
+
+            //driver.Quit();
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
